278380b231ea9fe548824b4ee9934387df191c4f
rambaldi <diencong5998@gmail.com>
2021-04-04 16:22:14 +0700

    max min 2 chieu

--- src/MaxMang2Chieu.java
+++ src/MaxMang2Chieu.java
@@ -19,8 +19,28 @@ public class MaxMang2Chieu {
         return a;
     }
 
+    public static String min(int[][] arr) {
+
+        int x = 0;
+        int y = 0;
+        int min = arr[0][0];
+        String a ="";
+        for(int i = 0; i < arr.length; i ++) {
+            for (int j = 0; j < arr[i].length; j ++) {
+                if(min > arr[i][j]){
+                    min = arr[i][j];
+                    x = i;
+                    y = j;
+                }
+
+            }
+        }
+        a = String.valueOf(min) + " " + String.valueOf(x) + " " +String.valueOf(y) ;
+        return a;
+    }
+
     public static void main(String[] args) {
-        String a = max( new int[][] {{1,2,3,4}, {5,6,7,8},{3,15,6}} );
+        String a = min( new int[][] {{1,2,3,4}, {-1,6,7,8},{3,15,6}} );
         System.out.println(a);
     }
 }
