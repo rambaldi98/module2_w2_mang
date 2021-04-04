f485125fbd3afc183cc7ffe8cd8540cbefec0b60
rambaldi <diencong5998@gmail.com>
2021-04-04 15:39:08 +0700

    chen mang

--- src/ThemMang.java
+++ src/ThemMang.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class ThemMang {
+public class ThemMang {
+
+    public static int[] them(int[] arr, int x, int index) {
+        int [] newarr = new int [arr.length + 1];
+        int dem = 0;
+        for(int  i = 0; i < arr.length; i ++) {
+            if(index == i){
+                dem ++;
+                newarr[i] = x;
+            }
+            newarr[i + dem] = arr[i];
+
+        }
+        return newarr;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(xoa(( new int[]{1,2,4 ,3,4,5}),4));
+        for(int x : them( new int[]{5,2,4,3,4,5},25,3)) {
+            System.out.println(x);
+        }
+
+    }
 }
