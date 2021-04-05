3addd20cb389c7c7e389afbf3c7b5502ea8aa90c
rambaldi <diencong5998@gmail.com>
2021-04-05 15:49:48 +0700

    hello

--- src/test.java
+++ src/test.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class test {
+public class test {
+
+    public static float chia2(float t){
+
+        return (float) (0.5) *t;
+    }
+    public static void main(String[] args) {
+        float c = (3/2)*9;
+        int d = (1/2)*9;
+        float  e = 5/4;
+        float f = 1/2;
+
+        float g = (float)0.5;
+        System.out.println(chia2(9));
+        System.out.println(f);
+        System.out.println(g);
+    }
 }
